03e2daa35f5c21a73537f5439cec66657c46af93
BuildTools <unconfigured@null.spigotmc.org>
2024-03-08 21:38:35 +0100

    Display Weather Data

--- app/src/main/java/fr/lucas/weatherapp/fragments/home/weatherDataAdapter.kt
+++ app/src/main/java/fr/lucas/weatherapp/fragments/home/weatherDataAdapter.kt
@@ -3,14 +3,17 @@ package fr.lucas.weatherapp.fragments.home
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import fr.lucas.weatherapp.data.CurrentLocation
+import fr.lucas.weatherapp.data.CurrentWeather
 import fr.lucas.weatherapp.data.weatherData
 import fr.lucas.weatherapp.databinding.ItemContainerCurrentLocationBinding
+import fr.lucas.weatherapp.databinding.ItemContainerCurrentWeatherBinding
 
 
 class weatherDataAdapter (
     private val onLocationClicked: () -> Unit
-) : RecyclerView.Adapter<weatherDataAdapter.CurrentLocationViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private companion object {
         const val INDEX_CURRENT_LOCATION = 0
@@ -33,22 +36,51 @@ class weatherDataAdapter (
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrentLocationViewHolder {
-        return CurrentLocationViewHolder(
-            ItemContainerCurrentLocationBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+    fun setCurrentWeather(currentWeather: CurrentWeather) {
+        if(weatherData.getOrNull(INDEX_CURRENT_WEATHER) != null) {
+            weatherData[INDEX_CURRENT_WEATHER]= currentWeather
+            notifyItemChanged(INDEX_CURRENT_WEATHER)
+        } else {
+            weatherData.add(INDEX_CURRENT_WEATHER, currentWeather)
+            notifyItemInserted(INDEX_CURRENT_WEATHER)
+        }
     }
 
-    override fun onBindViewHolder(holder: CurrentLocationViewHolder, position: Int) {
-        holder.bind(weatherData[position] as CurrentLocation)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            INDEX_CURRENT_LOCATION -> CurrentLocationViewHolder(
+                ItemContainerCurrentLocationBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+            else -> CurrentWeatherViewHolder(
+                ItemContainerCurrentWeatherBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+            is CurrentLocationViewHolder -> holder.bind(weatherData[position] as CurrentLocation)
+            is CurrentWeatherViewHolder -> holder.bind(weatherData[position] as CurrentWeather)
+        }
     }
 
     override fun getItemCount(): Int {
         return weatherData.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when(weatherData[position]) {
+            is CurrentLocation -> INDEX_CURRENT_LOCATION
+            is CurrentWeather -> INDEX_CURRENT_WEATHER
+        }
     }
 
     inner class CurrentLocationViewHolder(
@@ -66,6 +98,21 @@ class weatherDataAdapter (
             }
         }
 
+    inner class CurrentWeatherViewHolder(
+        private val binding: ItemContainerCurrentWeatherBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(currentWeather: CurrentWeather) {
+            with(binding) {
+                imageIcon.load("https:${currentWeather.icon}") { crossfade(true) }
+                textTemperature.text = String.format("%s\u00B0C",currentWeather.temperature)
+                textWind.text = String.format("%s km/h",currentWeather.wind)
+                textHumidity.text = String.format("%s%%",currentWeather.humidity)
+                textRain.text = String.format("%s%%",currentWeather.chanceOfRain)
+            }
+        }
     }
+
+
+}
 
 
